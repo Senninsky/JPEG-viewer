bb9ad88be5873a588cb8e89ef863ae0779bd857f
Senninsky <senne.vannieuwenhuyse@gmail.com>
2026-09-22 13:17:55 +0200

    little change in sos.h (more understanding)

--- sos.h
+++ sos.h
@@ -3,6 +3,11 @@
 
 typedef struct {
 	int length;
+	unsigned char Ns; // Tells how many image components participate in this scan: e.g. : 3 = Y, Cb, Cr
+	// Now there are 2 bytes per component: Cs | Td|Ta
+	unsigned char Cs;
+	unsigned char TdTa;
+	
 } SOS;
 
 SOS** getSOSs(unsigned char* buffer, long fileSize, int* amount_);
